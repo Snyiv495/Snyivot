1fc15a9ea5d4170a860abd8a9c558a33118928b9
Snyiv <snyiv495@gmail.com>
2025-10-21 21:24:52 +0900

    1.5.17 AIの不具合を修正

--- src/features/ai.js
+++ src/features/ai.js
@@ -16,7 +16,7 @@ const helper = require('../core/helper');
 const gemini = require('../integrations/gemini');
 
 //命令文の取得
-function getPrompt(trigger, log, reference, map, system_id){
+function getPrompt(trigger, reference, log, map, system_id){
     const gemini_prompt_json = JSON.parse(fs.readFileSync("./src/json/gemini-prompt.json", "utf-8"));
     const replacement = {"{{__TIME__}}": new Date().toLocaleString(), "{{__USER_NAME__}}": helper.getUserName(trigger), "{{__CHAT_LOG__}}": log, "{{__REFERENCE_MESSAGE__}}": reference, "{{__README__}}": map.get("readme_md")}
 
@@ -39,8 +39,8 @@ async function publicChat(message, map){
         const system_id = helper.getSystemId(message);
         const messages = [...(await message.channel.messages.fetch({limit: 21})).values()].sort((a, b) => a.createdTimestamp - b.createdTimestamp);
         const chat_log = JSON.stringify(messages.slice(0, -1).map(message => ({username: message.author.displayName, content: message.content, timestamp: message.createdAt.toISOString()})));
-        const reference_message = (await message.fetchReference())?.cleanContent ?? "null";
-        const gemini_res_function_call = (await gemini.genConFunc(message.content, getPrompt(message, chat_log, reference_message, map, system_id))).functionCalls?.[0];
+        const reference_message = message.reference ? (await message.fetchReference()).cleanContent : "null";
+        const gemini_res_function_call = (await gemini.genConFunc(message.content, getPrompt(message, reference_message, chat_log, map, system_id))).functionCalls?.[0];
 
         message.system_id = gemini_res_function_call?.name ?? "reply";
         message.args = gemini_res_function_call?.args ?? {"reply":"はっ ちょっとボ～っとしちゃってたよ ごめんね"}; 
